d5c1fe03deb5befaead3d4633facae82c1673334
lihiaq <lihiaq@gmail.com>
2019-01-27 22:08:51 +0200

    Add files via upload

---
+++ js/myJavaScript.js
@@ -0,0 +1,69 @@
+﻿
+
+//*************Change size bar*************
+
+
+
+$(document).ready(function () {
+
+    var toggleAffix = function (affixElement, scrollElement, wrapper) {
+
+        var height = affixElement.outerHeight(),
+            top = wrapper.offset().top;
+
+        if (scrollElement.scrollTop() >= top) {
+            wrapper.height(height);
+            affixElement.addClass("affix");
+        }
+        else {
+            affixElement.removeClass("affix");
+            wrapper.height('auto');
+        }
+
+    };
+
+
+    $('[data-toggle="affix"]').each(function () {
+        var ele = $(this),
+            wrapper = $('<div></div>');
+
+        ele.before(wrapper);
+        $(window).on('scroll resize', function () {
+            toggleAffix(ele, $(this), wrapper);
+        });
+
+        // init
+        toggleAffix(ele, $(window), wrapper);
+    });
+
+});
+
+//*************smooth scrolling func*************
+
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+
+    });
+});
+
+
